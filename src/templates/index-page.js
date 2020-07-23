0b7102936513786164bac7d209bc63f25866c74c
Jason Law <jasonl@criticalmass.com>
2020-07-22 21:46:10 -0400

    updating some design elements

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,9 +6,122 @@ import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
+import styled from 'styled-components'
+
+const HeroContainer = styled.div`
+	min-height: 672px;
+	display: flex;
+	justify-content: flex-start;
+	background-color: ${props => props.theme.basicBlue};
+
+`
+
+const Hero = styled.div`
+	margin: 125px auto 0 auto;
+	display: flex;
+	position: relative;
+`
+
+const HeroBox = styled.div`
+	width: 544px;
+	background-color: #ffffff;
+	height: 416px;
+	margin-right: 137px;
+`
+
+const HeroTagLine = styled.div`
+	font-family: ${props => props.theme.fontAccent};
+	font-size: 72px;
+	width: 401px;
+	color: #fff;
+`
+
+const HeroHeading = styled.h1`
+	font-family: ${props => props.theme.fontHeading};
+	margin: 90px 53px 13px 64px;
+	line-height: 69px;
+  font-size: 56px;
+	color: #3c4557;
+`
+const HeroSubHeading = styled.p`
+	margin: 0 53px 0 64px;
+`
+
+const HeroTitle = styled.div`
+	padding: 10px 20px;
+	background-color: #354463;
+	font-family: ${props => props.theme.fontBase};
+	font-size: 14px;
+	color: #fff;
+	position: absolute;
+	top: -20px;
+	left: 64px;
+	text-transform: uppercase;
+	font-weight: 300;
+	letter-spacing: 2px;
+`
+
+const Action = styled.div`
+	position: relative;
+	ul {
+		display: flex;
+		justify-content: space-around;
+		margin: 30px 0 0 0;
+		li {
+			list-style: none;
+			width: 225px;
+		}
+	}
+
+`
+
+const Heading = styled.div`
+	padding: 10px 20px;
+	position: absolute;
+	background-color: #3c4557;
+	font-family: ${props => props.theme.fontBase};
+	font-size: 14px;
+	color: #fff;
+	left: 45%;
+	top: -51px;
+	text-transform: uppercase;
+	font-weight: 300;
+	letter-spacing: 2px;
+
+	&.rel {
+		position: relative;
+		top: inherit;
+		left: inherit;
+		display: inline-block;
+
+		&.light {
+			background-color: white;
+			color: #3c4557;
+		}
+	}
+`
+
+const SectionDark = styled.div`
+	background-color: #3c4557;
+	padding: 86px 0;
+`
+
+const Content = styled.div`
+	width: 1024px;
+	margin: 0 auto;
+`
+
+const Headline = styled.h3`
+	font-family: ${props => props.theme.fontHeading};
+	color: white;
+	font-size: 44px;
+	line-height: 56px;
+`
+
 export const IndexPageTemplate = ({
   image,
-  title,
+	title,
+	featured,
   heading,
   subheading,
   mainpitch,
@@ -16,54 +129,64 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
-    <div
+    <HeroContainer
       className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
     >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
+			<Hero>
+				<HeroTitle>The Issue</HeroTitle>
+				<HeroBox>
+					<HeroHeading>
+						Suicide is a public health crisis
+					</HeroHeading>
+					<HeroSubHeading>
+						and Michigan families need our help.
+					</HeroSubHeading>
+				</HeroBox>
+				<HeroTagLine>
+					#Help PREVENT SUICIDE
+				</HeroTagLine>
+			</Hero>
+    </HeroContainer>
+		<Action>
+			<Heading>Take Action</Heading>
+			<ul>
+				<li>
+					<div>
+						<h3>Learn</h3>
+						<p>Get the facts about this issue and how we're helping</p>
+					</div>
+				</li>
+				<li>
+					<div>
+						<h3>Volunteer</h3>
+						<p>Find out about upcoming events that need your help</p>
+					</div>
+				</li>
+				<li>
+					<div>
+						<h3>Share</h3>
+						<p>Let your social media networks know about this important issue</p>
+					</div>
+				</li>
+				<li>
+					<div>
+						<h3>Donate</h3>
+						<p>Help us raise money to make a big difference</p>
+					</div>
+				</li>
+			</ul>
+		</Action>
+
+		<SectionDark>
+			<Content>
+				<Heading className={`rel light`}>
+					What We Do
+				</Heading>
+				<Headline>
+					Our mission is to support Michigan families in their time of greatest need, after losing a loved one to suicide.
+				</Headline>
+			</Content>
+		</SectionDark>
     <section className="section section--gradient">
       <div className="container">
         <div className="section">
@@ -133,7 +256,8 @@ const IndexPage = ({ data }) => {
     <Layout>
       <IndexPageTemplate
         image={frontmatter.image}
-        title={frontmatter.title}
+				title={frontmatter.title}
+				featured={frontmatter.featured}
         heading={frontmatter.heading}
         subheading={frontmatter.subheading}
         mainpitch={frontmatter.mainpitch}
@@ -159,6 +283,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+				featured
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
